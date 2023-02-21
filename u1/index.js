cde78b501cd64842dcc4ae47ff3f8e11ccafc3a4
rabiabee <rabiaahm25@gmail.com>
2023-02-21 19:22:17 +0100

    server status message box

--- u1/index.js
+++ u1/index.js
@@ -1,7 +1,9 @@
 "use strict";
 
+create_login_site();
+
 // event listener to "register" button
 const register_now = document.querySelector("#register_now");
-register_now.addEventListener("click", click_register);
+register_now.addEventListener("click", click_register_now);
 
 
--- u1/js/login_register.js
+++ u1/js/login_register.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function click_register() {
+function click_register_now() {
 
     //select login form element
     const main = document.querySelector("#main");
@@ -13,16 +13,19 @@ function click_register() {
         <input type="text" name="user_name" id="user_name">
         <label for="password">Password</label>
         <input type="password" name="password" id="password">
+        <p id="ready">Ready when you are...</p>
         <button type="submit" id="register_button">Register</button>
+        <p id="account_already">Already have an account? Go to login</p>
+
     </form>
 `;
 
     // click reg button sends post request
     const register_form = document.querySelector("#register_form");
-    register_form.addEventListener("submit", register_request);
+    register_form.addEventListener("submit", register_user_request);
 }
 
-async function register_request(event) {
+async function register_user_request(event) {
     event.preventDefault();
 
     const un = document.querySelector("#user_name").value;
@@ -34,13 +37,47 @@ async function register_request(event) {
         password: pw
     };
 
-    const options = {
+    const post = {
         method: "POST",
         headers: { "Content-type": "application/json; charset=UTF-8" },
         body: JSON.stringify(body_post),
     };
 
-    const request_post = await fetch_resource(new Request(prefix, options));
+    const request_post = await fetch_resource(new Request(prefix, post));
+
+    console.log(request_post.status);
+
+    // 409 = conflict
+    // 200 = success
+    // 418 = not a teapot 
+    // in a function? quiz answers
+    switch (request_post.status) {
+        case 409:
+            create_statusMessage_box("Sorry, that name is taken. Please try with another one.");
+            break;
+
+        case 200:
+            create_statusMessage_box("Registration Complete. Please proceed to login.");
+            break;
+
+        case 418:
+            create_statusMessage_box("The server thinks it's not a teapot!");
+            break;
+
+        default:
+            break;
+    }
+}
+function create_statusMessage_box(string) {
+    const status_box = document.body.appendChild(document.createElement("div"));
+    // server text response (error messages)
+    const status_box_text = document.createElement("p");
+    status_box_text.textContent = string;
+    status_box.appendChild(status_box_text);
+    // "close" button
+    const status_box_button = document.createElement("button");
+    status_box.appendChild(status_box_button);
+    status_box_button.textContent = "Close";
 }
 
 function create_login_site() {
@@ -55,6 +92,7 @@ function create_login_site() {
       <input type="text" name="user_name" id="user_name">
       <label for="password">Password</label>
       <input type="password" name="password" id="password">
+      <p id="magic_start">Let the magic start!</p>
       <button type="submit" id="login_button">Log in</button>
       <p id="register_now">New to this? Register for free</p>
 
